7bebbbda9f88c10e00209aabd2f49609c1582219
YULIANA ROLDAN CARVAJAL <yroldancarvajal@gmail.com>
2021-08-30 20:33:59 -0500

    Created table employed

---
+++ taller/taller.Common/Models/Employed.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace taller.Common.Models
+{
+    class Employed
+    {
+        public DateTime createdTime { get; set; }
+        public string TaskDescription { get; set; }
+        public bool IsComplete { get; set; }
+
+    }
+}
---
+++ taller/taller.Functions/Entities/EmployedEntity.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace taller.Functions.Entities
+{
+    class EmployedEntity
+    {
+        public int IdEmployed { get; set; }
+        public DateTime InputOutput { get; set; }
+        public int Type { get; set; }
+        public bool Consolidated { get; set; }
+    }
+}
